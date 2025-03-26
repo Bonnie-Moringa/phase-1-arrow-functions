c010d6699d8a4947cebf89b33e2d8487401a11b1
Boniface Mburu <boniface.mburu@student.moringaschool.com>
2025-03-26 12:33:23 +0300

    Square Arrow function

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const divide = function() {
     return 2000 / 100;
   };
+   console.log(divide());
   
-  // Test case
-  console.log(divide()); // Output: 20
-  
+const square = (num) => num * num;
+
+
+console.log(square(5)); 
+console.log(square(10)); 
